f0f82fc0dc16efd804f72f1a516d156acc3ee1d5
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 13:02:03 -0500

    test

--- dbbbot/__init__.py
+++ dbbbot/__init__.py
@@ -7,8 +7,8 @@ import sys
 from .py_v_check import py_v_check
 versionsuccess = py_v_check()
 if versionsuccess["errormessage"]:
-    print versionsuccess["errormessage"]
+    print (versionsuccess["errormessage"])
 if versionsuccess["error"]:
     sys.exit(1)
 else:
-    print "not exitting"
+    print ("not exitting")
