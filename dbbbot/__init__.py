b8c384222d5b3fd3db81be082c33032e7bbe3edf
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 12:15:49 -0500

    test

--- dbbbot/__init__.py
+++ dbbbot/__init__.py
@@ -4,8 +4,8 @@ dbbbot
 import sys
 
 # Check Python version is the supported version
-if sys.version_info != (3, 6):
-    print ('Error: Requires Python 3.6 or later.')
+if sys.version_info != (3, 5):
+    print ('Error: Testing for dbbbot has only been done on python version 3.5')
     sys.exit(1)
 
 from Mammals import Mammals
