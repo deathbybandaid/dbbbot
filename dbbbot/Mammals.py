3d91d60ce2ea14a46f650b767b3883cf76874a87
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 12:36:34 -0500

    test

--- dbbbot/Mammals.py
+++ dbbbot/Mammals.py
@@ -4,6 +4,7 @@ class Mammals:
         # Create some member animals
         self.members = ['Tiger', 'Elephant', 'Wild Cat']
 
+
     def printMembers(self):
         print('Printing members of the Mammals class')
         for member in self.members:
--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-# Import classes from your brand new package
 from dbbbot import Mammals
+
+# Create an object of Mammals class & call a method of it
+myMammal = Mammals()
+myMammal.printMembers()
