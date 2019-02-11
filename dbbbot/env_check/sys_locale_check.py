591198cb4ae07abf0ae18182040a30b5445260d0
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 13:39:30 -0500

    test

--- dbbbot/env_check/sys_locale_check.py
+++ dbbbot/env_check/sys_locale_check.py
@@ -15,10 +15,7 @@ def sys_locale_check():
     errordict = {"error": False, "errormessage": None}
 
     if not loc[1] or 'UTF-8' not in loc[1]:
-        if loc[1]:
-            errordict["errormessage"] = ('Error: dbbbot only functions on ' + str(supportedlocale) + " locale. You are running a non-UTF8 locale environment")
-        else:
-            errordict["errormessage"] = ('Error: dbbbot only functions on ' + str(supportedlocale) + " locale. Your locale is not set?")
+        errordict["errormessage"] = ('Error: dbbbot only functions on ' + str(supportedlocale) + " locale. You are running a non-UTF8 locale environment")
         errordict["error"] = True
     else:
         errordict["errormessage"] = ('Success: Locale is set to ' + str(loc[1]))
