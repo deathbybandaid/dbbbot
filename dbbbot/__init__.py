f89c438121fbc8709504b64cfd3846c896f2e95d
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 12:16:51 -0500

    test

--- dbbbot/__init__.py
+++ dbbbot/__init__.py
@@ -4,6 +4,7 @@ dbbbot
 import sys
 
 # Check Python version is the supported version
+print sys.version_info
 if sys.version_info != (3, 5):
     print ('Error: Testing for dbbbot has only been done on python version 3.5')
     sys.exit(1)
