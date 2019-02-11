5463bdc11db8056ba4a782b279423b4a7341e42b
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 13:17:36 -0500

    test

--- dbbbot/env_check/py_v_check.py
+++ dbbbot/env_check/py_v_check.py
@@ -13,7 +13,7 @@ def py_v_check():
 
     # Check Python version is the supported version
     if sys.version_info[0] != python_version_major or sys.version_info[1] != python_version_minor:
-        errordict["errormessage"] = ('Error: dbbbot only functions on ' + str(python_version_major) + "." + str(python_version_minor))
+        errordict["errormessage"] = ('Error: dbbbot only functions on ' + str(python_version_major) + "." + str(python_version_minor) + " Your version is " + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
         errordict["error"] = True
     else:
         errordict["errormessage"] = ('Success: Python version is ' + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
