8696fd86459ce3c48986e507b6fd1a0cef281f46
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 13:24:21 -0500

    test

--- dbbbot/common.py
+++ dbbbot/common.py
@@ -3,49 +3,12 @@ Display Functions
 """
 
 
-def dbb_avatar(dbbparse):
-    filepath = os.path.join(dbbparse.paths['scripts_common'], 'avatar.txt')
-    lines = [line.rstrip('\n') for line in open(filepath)]
-    osd(textarray=lines)
+def dprint_divider(color='BOLD'):
+
+    dprint(textarray=["___________________________________________________________"], color='BOLD')
 
 
-def humanized_time(countdownseconds):
-    time = float(countdownseconds)
-    if time == 0:
-        return "0 seconds"
-    year = time // (365 * 24 * 3600)
-    time = time % (365 * 24 * 3600)
-    day = time // (24 * 3600)
-    time = time % (24 * 3600)
-    time = time % (24 * 3600)
-    hour = time // 3600
-    time %= 3600
-    minute = time // 60
-    time %= 60
-    second = time
-    displaymsg = None
-    timearray = ['year', 'day', 'hour', 'minute', 'second']
-    for x in timearray:
-        currenttimevar = eval(x)
-        if currenttimevar >= 1:
-            timetype = x
-            if currenttimevar > 1:
-                timetype = str(x+"s")
-            if displaymsg:
-                displaymsg = str(displaymsg + " " + str(int(currenttimevar)) + " " + timetype)
-            else:
-                displaymsg = str(str(int(currenttimevar)) + " " + timetype)
-    if not displaymsg:
-        return "0 seconds"
-    return displaymsg
-
-
-def dividerbar(color='BOLD'):
-
-    osd(textarray=["___________________________________________________________"], color='BOLD')
-
-
-def osd(textarray=[], indent=0, color='BOLD'):
+def dprint(textarray=[], indent=0, color='BOLD'):
 
     if not isinstance(textarray, list):
         textarray = [str(textarray)]
@@ -66,4 +29,4 @@ def osd(textarray=[], indent=0, color='BOLD'):
             if not isinstance(indent, int):
                 indent = 1
             indentstr = "     " * indent
-        print indentstr + coloreval + entry + endcolor
+        print (indentstr + coloreval + entry + endcolor)
