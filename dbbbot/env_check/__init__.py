cfa25c73d21c7a8eebefd9ffec02f01c85d9ef74
deathbybandaid <sam@deathbybandaid.net>
2019-02-11 13:29:25 -0500

    test

--- dbbbot/env_check/__init__.py
+++ dbbbot/env_check/__init__.py
@@ -10,6 +10,9 @@ import sys
 from .py_v_check import py_v_check
 versionsuccess = py_v_check()
 if versionsuccess["errormessage"]:
-    dprint(versionsuccess["errormessage"])
+    if versionsuccess["error"]:
+        dprint(versionsuccess["errormessage"], color='RED')
+    else:
+        dprint(versionsuccess["errormessage"], color='GREEN')
 if versionsuccess["error"]:
     sys.exit(1)
