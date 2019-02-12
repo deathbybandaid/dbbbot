3d1cb7d12b0f7703fb019975a2a75837c937d43e
deathbybandaid <sam@deathbybandaid.net>
2019-02-12 08:04:55 -0500

    test spacing

--- dbbbot/env_check/__init__.py
+++ dbbbot/env_check/__init__.py
@@ -9,17 +9,22 @@ import sys
 
 dprint_divider(color='BLUE')
 
+dprint(["Performing Environment Checks", ""])
+
+dprint("Checking Python Version")
 from .py_v_check import py_v_check
 versionsuccess = py_v_check()
 if versionsuccess["errormessage"]:
     dprint(versionsuccess["errormessage"], color=dprint_error_color(versionsuccess["error"]))
 if versionsuccess["error"] and versionsuccess["error"] != "WARN":
     sys.exit(1)
+dprint("")
 
-
+dprint("Checking System Locale")
 from .sys_locale_check import sys_locale_check
 localesuccess = sys_locale_check()
 if localesuccess["errormessage"]:
     dprint(localesuccess["errormessage"], color=dprint_error_color(localesuccess["error"]))
 if localesuccess["error"] and localesuccess["error"] != "WARN":
     sys.exit(1)
+dprint("")
