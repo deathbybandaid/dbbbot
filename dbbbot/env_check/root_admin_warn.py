c1a92d6a7b993bd519977ee0d6f5ef4e7312837f
deathbybandaid <sam@deathbybandaid.net>
2019-02-12 09:01:45 -0500

    test

--- dbbbot/env_check/root_admin_warn.py
+++ dbbbot/env_check/root_admin_warn.py
@@ -8,8 +8,29 @@ import platform
 
 def root_admin_warn():
     errordict = {"error": False, "errormessage": None}
-    print (platform.system())
-    print (os.name)
+
+    # Detect operating system
+    opersystem = platform.system()
+
+    # Linux: Linux
+    # Mac: Darwin
+    # Windows: Windows
+
+    if opersystem in ["Linux", "Darwin"]:
+        if os.getuid() == 0 or os.geteuid() == 0:
+            errordict["errormessage"] = ("Warning: dbbbot should not be run on root")
+            errordict["error"] = "WARN"
+        else:
+            errordict["errormessage"] = ("Succese: dbbbot is not run on root")
+    elif opersystem in ["Windows"]:
+        if os.environ.get("USERNAME") == "Administrator":
+            errordict["errormessage"] = ("Warning: dbbbot should not be run on administrator")
+            errordict["error"] = "WARN"
+        else:
+            errordict["errormessage"] = ("Succese: dbbbot is not run on administrator")
+    else:
+        errordict["errormessage"] = ("ERROR: Could not detect Operating Sytem Type, or it is unsupported.")
+        errordict["error"] = True
 
     return errordict
 
