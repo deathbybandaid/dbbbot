7b9c78e8552727da4a8bb86bcd7f07692061fa57
deathbybandaid <sam@deathbybandaid.net>
2019-02-12 08:40:29 -0500

    test

--- dbbbot/args_handler/__init__.py
+++ dbbbot/args_handler/__init__.py
@@ -12,22 +12,18 @@ dprint_divider(color='BLUE')
 
 dprint(["Checking For CLI Arguments", ""])
 
-
-inputfile, outputfile = '', ''
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["ifile=", "ofile="])
-except getopt.GetoptError:
-    print ('test.py -i <inputfile> -o <outputfile>')
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print ('test.py -i <inputfile> -o <outputfile>')
-        sys.exit()
-    elif opt in ("-i", "--ifile"):
-        inputfile = arg
-    elif opt in ("-o", "--ofile"):
-        outputfile = arg
-print ('Input file is "', inputfile)
-print ('Output file is "', outputfile)
+parser = build_parser()
+opts = parser.parse_args(sys.argv[1:] or None)
+print (opts)
 
 dprint("")
+
+
+def build_parser():
+    """Build an ``argparse.ArgumentParser`` for the bot"""
+
+    parser = argparse.ArgumentParser(description='dbbbot', usage='%(prog)s [options]')
+
+    parser.add_argument('-v', '--version', action="store_true", dest="version", help="Show version number and exit")
+
+    return parser
